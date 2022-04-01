c9f466fb4ad058c7ebe5bac21b28f56e5609bf65
1ntellekt <pitetvasa@gmail.com>
2022-04-01 17:42:15 +0600

    commit chatObserve ModelChange

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -183,7 +183,7 @@ class UserController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'Successfully logged out'
-        ]);
+        ],200);
 
     }
 
